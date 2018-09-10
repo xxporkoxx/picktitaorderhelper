09b02db36e754fe2ec766c2cbf90ad920dcccd4c
Diego Mello <diego@picktita.com.br>
2018-09-09 21:39:54 -0300

    adding all the correect columns

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,35 +1,98 @@
 import React, { Component } from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
 import filterFactory, { textFilter, Comparator } from 'react-bootstrap-table2-filter';
+import cellEditFactory from 'react-bootstrap-table2-editor';
+import ToolkitProvider, { CSVExport } from 'react-bootstrap-table2-toolkit';
+
+const { ExportCSVButton } = CSVExport;
 
 const columns = [{
-    dataField: 'id',
-    text: 'Product ID'
-  }, {
-    dataField: 'name',
-    text: 'Product Name'
-  }, {
-    dataField: 'price',
-    text: 'Product Price'
-  }];
+  dataField: 'reference',
+  text: 'Ref'
+}, {
+  dataField: 'quantity',
+  text: 'Qnt'
+}, {
+  dataField: 'color',
+  text: 'Cor'
+}, {
+  dataField: 'p',
+  text: 'P'
+}, {
+  dataField: 'm',
+  text: 'M'
+}, {
+  dataField: 'g',
+  text: 'G'
+}, {
+  dataField: 'um',
+  text: '1'
+}, {
+  dataField: 'duas',
+  text: '2'
+}, {
+  dataField: 'tres',
+  text: '3'
+}, {
+  dataField: 'quatro',
+  text: '4'
+}, {
+  dataField: 'seis',
+  text: '6'
+}, {
+  dataField: 'oito',
+  text: '8'
+}, {
+  dataField: 'dez',
+  text: '10'
+}, {
+  dataField: 'doze',
+  text: '12'
+}, {
+  dataField: 'quatorze',
+  text: '14'
+}, {
+  dataField: 'price',
+  text: 'Preço'
+}, {
+  dataField: 'total',
+  text: 'Total'
+}];
 
 const products = [{
-    id: 1,
-    name: "Product1",
-    price: 120
+  reference: 1,
+  name: "Product1",
+  price: 120
 }, {
-    id: 2,
-    name: "Product2",
-    price: 80
+  reference: 2,
+  name: "Product2",
+  price: 80
 }];
 
 export class Dashboard extends Component {
-    render() {
+  render() {
     return (
       <div>
-        <p>Dashboard</p>
-        <BootstrapTable keyField='id' data={ products }
-         columns={ columns } filter={ filterFactory() } />
+        <ToolkitProvider
+          keyField="id"
+          data={products}
+          columns={columns}
+          exportCSV
+        >
+          {
+            props => (
+              <div>
+                <ExportCSVButton {...props.csvProps}>Baixar relatório</ExportCSVButton>
+                <hr />
+                <BootstrapTable 
+                  {...props.baseProps}
+                  filter={filterFactory()}
+                  cellEdit={cellEditFactory({ mode: 'click' })}
+                />
+              </div>
+            )
+          }
+        </ToolkitProvider>
       </div>
     )
   }
