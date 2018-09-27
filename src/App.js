6103307592d5ef28eac901fb8dc57b97a81607ca
Diego Mello <diego@picktita.com.br>
2018-09-26 21:24:45 -0300

    Forcing redirect to Dashboard on root page access, changeing logo and adding navBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Route, Link } from "react-router-dom";
-import {Row, Col, Image} from 'react-bootstrap';
+import { Route, Redirect } from "react-router-dom";
+import { Image, Nav, Navbar, NavItem } from 'react-bootstrap';
 import Dashboard from './Dashboard'
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 
-const cssImport = <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
-const cssStylesheet = <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous"/>
+const cssImport = <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
+const cssStylesheet = <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous" />
 
 class App extends Component {
   render() {
@@ -14,20 +14,25 @@ class App extends Component {
       <div>
         {cssImport}
         {cssStylesheet}
-        <Row>
-          <Col xs={6} md={6}>
-            <Image src="/assets/logo-picktita.png"/>
-          </Col>
-          <Col xs={6} md={6}>
-            <nav>
-              <Link to="/dashboard">Dashboard</Link>
-            </nav>
-          </Col>
-        </Row>
-      <div>
-        <Route path="/dashboard" component={Dashboard}/>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="/dashboard">
+                <Image src={"/assets/picktita-logo-fundo.png"} style={{ width: 100, marginTop: -7 }} />
+              </a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <NavItem eventKey={1} href="/newFeature">
+              Nova Funconalidade
+            </NavItem>
+          </Nav>
+        </Navbar>
+        <Redirect from="/" to="/dashboard" />
+        <div>
+          <Route path="/dashboard" component={Dashboard} />
+        </div>
       </div>
-    </div>  
     );
   }
 }
