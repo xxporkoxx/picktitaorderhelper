48b8c02766b56486817c9842e351c3b05a36df07
xxporkoxx <diegomello@tokenlab.com.br>
2018-12-22 20:54:47 -0200

    Implementing React Redux Pattern

--- src/TrayIntegration.js
+++ src/TrayIntegration.js
@@ -1,19 +1,50 @@
 import { Button } from 'react-bootstrap';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { clickButton } from './actions';
 
 export class TrayIntegration  extends Component {
 
     constructor(props) {
       super(props);
+       let initialState={
+           inputValue: ''
+       }
+       this.state = { initialState };
     }
 
+    inputChange = event => {
+        this.setState({
+          inputValue: event.target.value
+        })
+    } 
+
     render(){
+        const {newValue, clickButton} = this.props;
+        const { inputValue } = this.state;
+
         return(
-            <Button>
-                Incluir arquivo
-            </Button>
+            <div style={{ paddingTop: '10px' }}>
+                <input
+                    onChange={this.inputChange}
+                    type='text'
+                    value={inputValue}
+                />
+                <Button onClick={() => clickButton(inputValue)}>
+                    Incluir arquivo
+                </Button>
+                <h1>{newValue}</h1>
+          </div>
         );
     }
 }
 
-export default TrayIntegration
+const mapStateToProps = store => ({
+    newValue: store.clickState.newValue
+});
+
+const mapDispatchToProps = dispatch =>
+    bindActionCreators({ clickButton }, dispatch);
+  
+export default connect(mapStateToProps, mapDispatchToProps)(TrayIntegration);
--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { Store } from './store';
 
 ReactDOM.render((
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={Store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 ), document.getElementById('root'));
+
 registerServiceWorker();
