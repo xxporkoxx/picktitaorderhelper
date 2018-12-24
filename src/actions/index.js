a9d0b30927480ab2862ee367683717c6e033d2eb
xxporkoxx <diegomello@tokenlab.com.br>
2018-12-24 13:11:38 -0200

    Requesting the acces token

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,23 +1,43 @@
-import {TRAY_CODE, TRAY_CONSUMER_KEY, TRAY_CONSUMER_SECRET_KEY } from '../api_keys';
+import {TRAY_CODE, TRAY_CONSUMER_KEY, TRAY_CONSUMER_SECRET_KEY, TRAY_API_URL } from '../api_keys';
+import axios from 'axios';
 
 export const CLICK_UPDATE_VALUE = 'CLICK_UPDATE_VALUE';
-export const clickButton = value => ({
-    type: CLICK_UPDATE_VALUE,
-    newValue: value
-})
+export const clickButton = () => {
+    let data = {
+        code: TRAY_CODE,
+        consumer_key: TRAY_CONSUMER_KEY,
+        consumer_secret: TRAY_CONSUMER_SECRET_KEY
+    };
+
+    return (dispatch) => {
+        return axios.post(`${TRAY_API_URL}/auth`,data)
+        .then(response => {
+            console.log(response);
+        })
+        .catch(error => {
+            console.log(error.data);
+        });
+    }
+}
 
 export const TRAY_AUTH_POST = 'TRAY_AUTH_POST';
-export const tray_auth = () =>({
-    type: TRAY_AUTH_POST
-})
+export const tray_auth = () =>{
+    return {
+        type: TRAY_AUTH_POST
+    }
+}
 
 export const TRAY_AUTH_SUCCESS = 'TRAY_AUTH_SUCCESS';
-export const tray_auth_success = () =>({
-    type: TRAY_AUTH_SUCCESS
-})
+export const tray_auth_success = () =>{
+    return {
+        type: TRAY_AUTH_SUCCESS
+    }
+}
 
 
 export const TRAY_AUTH_FAILURE = 'TRAY_AUTH_FAILURE';
-export const tray_auth_failure = () =>({
-    type: TRAY_AUTH_FAILURE
-})
+export const tray_auth_failure = () =>{
+    return {
+        type: TRAY_AUTH_FAILURE
+    }
+}
--- src/api_keys.js
+++ src/api_keys.js
@@ -19,8 +19,9 @@
 // https://api.commerce.tray.com.br/app/webroot/api-docs/
 
 export const TRAY_CODE = '9c5880536cf171d4a49805987ae7cfa3cb7241b4eb40d2311e7a35a48f57f12a';
-export const TARY_CONSUMER_KEY = '1cac5361a85b1c8043f0a93d8ae093bf3c643b2f414780b8b95dab621c775124';
+export const TRAY_CONSUMER_KEY = '1cac5361a85b1c8043f0a93d8ae093bf3c643b2f414780b8b95dab621c775124';
 export const TRAY_CONSUMER_SECRET_KEY = 'bbee3d024b152c74c86c7c338df0d695b0e7cf71aa8c1416b246a6adc1fa1321';
 export const TARY_STORE_NUMBER = 391250;
 export const TRAY_USER = 'conta_teste';
 export const TRAY_PASSWORD = 'm3n4cp97';
+export const TRAY_API_URL = 'https://trayparceiros.commercesuite.com.br/web_api';
